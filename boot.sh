17e117b0076805b32a624c9ad838f88fcd6d3f4f
Elie Gavoty <eliegavoty@free.fr>
2021-12-23 00:53:20 +0100

    fix boot.sh again

--- boot.sh
+++ boot.sh
@@ -2,8 +2,8 @@
 set -e
 if [ "$CONTEXT" = 'DEV' ]; then
     echo "Running Development Server"
-    exec python3 "/home/flask/src/monster_icon.py"
+    exec python3 "/home/flask/app/monster_icon.py"
 else
     echo "Running Production Server"
-    exec uwsgi --http 0.0.0.0:9090 --wsgi-file /home/flask/src/monster_icon.py --callable app --stats 0.0.0.0:9191
+    exec uwsgi --http 0.0.0.0:9090 --wsgi-file /home/flask/app/monster_icon.py --callable app --stats 0.0.0.0:9191
 fi
